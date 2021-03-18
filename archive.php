7b5828eb58f8ed7862717275795bf8397590aea8
Alex Wardle <61836432+XND3000@users.noreply.github.com>
2021-03-18 19:13:33 +0000

    Created Sidebars For Widgets

--- archive.php
+++ archive.php
@@ -2,25 +2,65 @@
 
 <section class="page-wrap">
 <div class="container">
+
+
+
+		<section class="row">
+
+			<div class="col-lg-3">
+
+
+
+
+
+
+
+			<?php if (is_active_sidebar('blog-sidebar') ):?>
+
+			<?php dynamic_sidebar('blog-sidebar');?> 
+
+			<?php endif;?>
+
+
+
+
+
+		</div>
+
+
+		<div class="col-lg-9">
+
+
+
+
+
+
+
+
 	
 				<h1><?php echo single_cat_title();?></h1>
 
 				<?php get_template_part('includes/section','archive');?>
 
-				<?php 
-				global $wp_query;
-				$big = 999999999; //need an unlikely integer
+				<?php previous_posts_link();?>
+				<?php next_posts_link(); 
 
-				echo paginate_links(array(
-					'base' => str_replace($big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-					'format' => '?paged=%#%',
-					'current' => max(1, get_query_var('paged') ),
-					'total' => $wp_query->max_num_pages
-				));
-				?>
+
+				//global $wp_query;
+				//$big = 999999999; //need an unlikely integer
+
+				//echo paginate_links(array(
+				//	'base' => str_replace($big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+				//	'format' => '?paged=%#%',
+				//	'current' => max(1, get_query_var('paged') ),
+				//	'total' => $wp_query->max_num_pages
+			//	));
+			//	?>
 			
+				</div>
 
-				<?php wp_link_pages();?>
+			</section>
+
 
 
 
--- functions.php
+++ functions.php
@@ -39,6 +39,8 @@ add_action('wp_enqueue_scripts','load_js');
 // theme options
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
+add_theme_support('widgets');
+
 
 
 
@@ -61,6 +63,66 @@ register_nav_menus(
 // custom image sizes 
 add_image_size('blog-large', 800,400,true);
 add_image_size('blog-small', 300,200,true);
+
+
+
+
+// register sidebars
+function my_sidebars()
+{
+
+		register_sidebar(
+
+					array(
+
+
+						'name' => 'Page Sidebar',
+						'id' => 'page-sidebar',
+						'before_title' => '<h4 class="widget-title">',
+						'after_title' => '</h4>'
+
+
+
+					)
+
+
+
+		);
+
+			register_sidebar(
+
+					array(
+
+
+						'name' => 'Blog Sidebar',
+						'id' => 'blog-sidebar',
+						'before_title' => '<h4 class="widget-title">',
+						'after_title' => '</h4>'
+
+
+
+					)
+
+
+
+		);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+add_action('widgets_init','my_sidebars');
 
 
 
